9b709555d762aed79ed6d3bfae627d766938fd09
Geoff Kline <spifford@gmail.com>
2022-03-31 21:40:56 -0500

    Shield class seems to be working

--- Python/Shield.py
+++ Python/Shield.py
@@ -1,4 +1,5 @@
 import pygame;
+import enum;
 
 class Shield:
     """
@@ -12,15 +13,28 @@ class Shield:
             denotes the horizontal size of a text character from the original Commodore 64 game
     shield_block_size_y : int
             denotes the verticle size of a text character from the original Commodore 64 game
-
+    shield_screen_y_location : int
+            denotes the y location on the screen where the shield should be placed
+    shield_screen_max_y : int
+            denotes the max y coordinate where the shield should start to draw
+    shield_direction : int
+            denotes which direction the shield should be moving
+    shield_surface : pygame.Surface
+            Contains the surface object from pygame which the shield is drawn on
+    blue = tuple(int,int,int)
+            denotes the RGB value of the color Blue
     
     Methods
     -------
     draw_shield(surface):
         Draws the current shield location to the screen
 
+    get_shield():
+        Returns the surface object containing the shield
+
+    get_shield_screen_y():
+        Returns an integer of which y coordinate to start drawing the shield at
     """
-    
     def __init__(self):
         """
         Contructs all the necessary attributes for Qotile's shield object.
@@ -38,9 +52,17 @@ class Shield:
                              [0,0,0,0,4,2,5]]
         self.shield_block_size_x = 8
         self.shield_block_size_y = 8
+        self.shield_screen_y_location = 0
+        self.shield_screen_max_y = 144
+        self.shield_direction = 1
         self.shield_surface = pygame.Surface((56,200))
+        self.blue = (0,0,255)
 
     def draw_shield(self):
+        """
+        Moves the shield, and draws the shield graphic onto the surface object
+        """
+        self.__move_shield()
         for y in range(len(self.shield_layout)):
             for x in range(len(self.shield_layout[0])):
                 point_a = (((x*self.shield_block_size_x)+7),(y*self.shield_block_size_y))
@@ -48,17 +70,38 @@ class Shield:
                 point_c = (((x*self.shield_block_size_x)+7),((y*self.shield_block_size_y)+7))
                 point_d = ((x*self.shield_block_size_x),(y*self.shield_block_size_y))
                 if self.shield_layout[y][x]==1:
-                    pygame.draw.polygon(self.shield_surface, (0,0,255), (point_a,point_c,point_b))
+                    pygame.draw.polygon(self.shield_surface, self.blue, (point_a,point_c,point_b))
                 if self.shield_layout[y][x]==2:
-                    pygame.draw.rect(self.shield_surface, (0,0,255), (x*self.shield_block_size_x,y*self.shield_block_size_y,self.shield_block_size_x,self.shield_block_size_y))
+                    pygame.draw.rect(self.shield_surface, self.blue, (x*self.shield_block_size_x,y*self.shield_block_size_y,self.shield_block_size_x,self.shield_block_size_y))
                 if self.shield_layout[y][x]==3:
-                    pygame.draw.polygon(self.shield_surface, (0,0,255), (point_d,point_a,point_b))
+                    pygame.draw.polygon(self.shield_surface, self.blue, (point_d,point_a,point_b))
                 if self.shield_layout[y][x]==4:
-                    pygame.draw.polygon(self.shield_surface, (0,0,255), (point_d,point_a,point_c))
+                    pygame.draw.polygon(self.shield_surface, self.blue, (point_d,point_a,point_c))
                 if self.shield_layout[y][x]==5:
-                    pygame.draw.polygon(self.shield_surface, (0,0,255), (point_d,point_c,point_b))                    
+                    pygame.draw.polygon(self.shield_surface, self.blue, (point_d,point_c,point_b))     
+
     def get_shield(self):
+        """
+        Returns the surface object containin the shield
+        """
         return self.shield_surface
+    
+    def get_shield_screen_y(self):
+        """
+        Returns an integer that denotes the y-coordinate to blit the shield to
+        """
+        return self.shield_screen_y_location
+    
+    def __move_shield(self):
+        """
+        Moves the shield and corrects the direction of movement when screen limits are reached
+        """
+        if self.shield_screen_y_location<=0:
+            self.shield_direction = 1
+        if self.shield_screen_y_location>=self.shield_screen_max_y:
+            self.shield_direction = -1
+        self.shield_screen_y_location = self.shield_screen_y_location + (8 * self.shield_direction)
+
 
 
     
--- Python/YarsRevival.py
+++ Python/YarsRevival.py
@@ -1,31 +1,29 @@
 import sys, pygame
 from Shield import Shield
+import time
 
 pygame.init()
 
 size = width, height = 320, 240
 speed = [2, 2]
-black = 0, 0, 0
+black = (0, 0, 0)
 
+# Initialize the screen 
 screen = pygame.display.set_mode(size)
+screen.fill(black)
 
+# Initialize the classes
+# Create Qotile's shield class instance
 qotile_shield = Shield()
 qotile_shield.draw_shield()
 
-#ball = pygame.image.load("images/intro_ball.gif")
-#ballrect = ball.get_rect()
-
-
+# Enter the repeating game loop
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    #ballrect = ballrect.move(speed)
-    #if ballrect.left < 0 or ballrect.right > width:
-    #    speed[0] = -speed[0]
-    #if ballrect.top < 0 or ballrect.bottom > height:
-    #    speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(qotile_shield.get_shield(), (264,0) )
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill((0,0,0))
+    qotile_shield.draw_shield()
+    screen.blit(qotile_shield.get_shield(), (264,qotile_shield.get_shield_screen_y()) )
     pygame.display.flip()
+    #time.sleep(0.1)
